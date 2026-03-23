57d7fdb04a992b626438c5520f5307f2abfa2c88
Breno Martins <crdj161@gmail.com>
2026-03-23 13:39:56 -0300

    Add base math

---
+++ mathlib.py
@@ -0,0 +1,14 @@
+import math
+
+def calc_oddies(tries: int, chance: float):
+   return 1 - (1-chance)**tries
+
+
+def calc_tries(percent: float, chance: float):
+    return round((math.log(1-percent))/(math.log(1-chance)))
+
+
+if __name__=='__main__':
+    print(calc_oddies(tries=50, chance=1/100)*100)
+    print(calc_tries(0.395, 1/100))
+    
